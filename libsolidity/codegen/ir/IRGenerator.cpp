35564db736ffba094d96528c907c8e3849be6331
Nikola Matic <nikola.matic@ethereum.org>
2024-07-22 12:43:17 +0200

    Via-IR code-gen for transient state variables
    Alternative approach

--- libsolidity/codegen/ir/IRGenerator.cpp
+++ libsolidity/codegen/ir/IRGenerator.cpp
@@ -834,7 +834,6 @@ std::string IRGenerator::initStateVariables(ContractDefinition const& _contract)
 	IRGeneratorForStatements generator{m_context, m_utils, m_optimiserSettings};
 	for (VariableDeclaration const* variable: _contract.stateVariables())
 	{
-		solUnimplementedAssert(variable->referenceLocation() != VariableDeclaration::Location::Transient, "Transient storage variables not supported.");
 		if (!variable->isConstant())
 			generator.initializeStateVar(*variable);
 	}
